750011892bf1cf40233fed4462728e3c654845ec
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2025-05-03 15:32:11 +0530

    Update SearchRepository.java

--- src/main/java/com/barath/joblisting/repository/SearchRepository.java
+++ src/main/java/com/barath/joblisting/repository/SearchRepository.java
@@ -2,11 +2,7 @@ package com.barath.joblisting.repository;
 
 import com.barath.joblisting.model.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import java.util.List;
 
-public interface SearchRepository extends MongoRepository<Post, String> {
-
-    @Query("{ '$text': { '$search': ?0 } }")
-    List<Post> findByText(String text);
+public interface SearchRepository extends MongoRepository<Post, String>, SearchRepositoryCustom {
+    // keep it clean here; move findByText to custom interface
 }
